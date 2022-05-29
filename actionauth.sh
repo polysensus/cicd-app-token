dc89b3b661c705f0a400747b65d4ad8d9bd6aa9f
Robin Bryce <robinbryce@gmail.com>
2022-05-29 12:01:23 +0100

    deal with absence of jq (vercel build env)

--- actionauth.sh
+++ actionauth.sh
@@ -5,6 +5,18 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 JSON=$($SCRIPT_DIR/accesstoken.sh $INPUT_APP_ID $INPUT_APP_PEM true)
 
+# Because vercel's build environment doesn't have jq
+JQ=$(which jq)
+if [ -z "$JQ" ]; then
+  mkdir -p bin
+  curl  https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -sL --output bin/jq
+  chmod a+x bin/jq
+  JQ=$(pwd)/bin/jq
+  echo "jq installed: $JQ"
+  export PATH=$PATH:$(pwd)/bin
+fi
+echo "JQ: $JQ"
+
 echo $JSON | jq -r '.permissions'
 TOKEN=$(echo -n $JSON | jq -r '.token')
 
